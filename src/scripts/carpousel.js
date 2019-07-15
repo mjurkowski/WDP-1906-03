288186986d68c54c32dbc2617e8839b33cee0451
Grzegorz Turek <grzegorz.s.turek@gmail.com>
2019-07-15 15:34:56 +0200

    Init commit

---
+++ src/scripts/carpousel.js
@@ -0,0 +1,17 @@
+/* global Flickity */
+
+var elem = document.querySelector('.main-carousel');
+// var flkty = new Flickity(elem, {
+Flickity(elem, {
+  // options
+  cellAlign: 'left',
+  contain: true
+});
+
+/*
+// element argument can be a selector string
+//   for an individual element
+flkty = new Flickity('.main-carousel', {
+  // options
+});
+*/
