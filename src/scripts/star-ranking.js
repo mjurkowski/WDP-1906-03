cd0abd984c353cc7cd0ab65fdcc4db4043415024
Andrzej Mieczkowski <praca.mieczkowski@gmail.com>
2019-07-16 09:43:45 +0200

    event and i errors removed

--- src/scripts/star-ranking.js
+++ src/scripts/star-ranking.js
@@ -1,12 +1,15 @@
-(function() {
-  function mySuperFunc() {
+(function () {
+  var i;
+
+  function mySuperFunc (event) {
     event.preventDefault();
 
     var parent = event.target.parentNode;
     var boxDataRate = event.target.getAttribute('data-rate');
     var stars = parent.getElementsByTagName('star');
+    var i;
 
-    for (var i = 0; i < stars.length; i++) {
+    for (i = 0; i < stars.length; i++) {
       var star = stars[i];
       var value = star.getAttribute('data-rate');
       if (value <= boxDataRate) {
@@ -19,26 +22,28 @@
 
   // Checking clicked star...
   var allStars = document.querySelectorAll('star');
-  for (var i = 0; i < allStars.length; i++) {
+  for (i = 0; i < allStars.length; i++) {
     allStars[i].addEventListener('click', mySuperFunc);
   }
 
   // Box's with default class fill ranking
   var productBox = document.querySelectorAll('star-rating');
 
-  for (var i = 0; i < productBox.length; i++) {
+  for (i = 0; i < productBox.length; i++) {
     var box = productBox[i];
     setDefaultStars(box);
   }
 
-  function setDefaultStars(box) {
+  function setDefaultStars (box) {
     var ratingValue = box.getAttribute('default');
+    var i;
+
     if (ratingValue <= 0) {
       return;
     }
     var stars = box.getElementsByTagName('star');
     var upperBound = Math.min(stars.length, ratingValue);
-    for (var i = 0; i < upperBound; i++) {
+    for (i = 0; i < upperBound; i++) {
       stars[i].classList.add('fill');
     }
   }
